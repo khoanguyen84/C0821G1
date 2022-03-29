48adf92811617e6ab77efb312abcd30be22b403b
Khoa Nguyen <khoa.nguyen@lbhpartners.com>
2022-03-29 17:35:43 +0700

    JSX

--- Module5/ReactJSBasic/reactjs.js
+++ Module5/ReactJSBasic/reactjs.js
@@ -1,17 +1,26 @@
-const h1 =  React.createElement(
-    "h1",
-    {
-        className: "heading-1",
-        title: "This is heading 1"
-    },
-    "Heading 1");
+// const h1 =  React.createElement(
+//     "h1",
+//     {
+//         className: "heading-1",
+//         title: "This is heading 1"
+//     },
+//     "Heading 1");
 
-const p = React.createElement("p", null, "Đây là nội dung của thẻ P");
+// const p = React.createElement("p", null, "Đây là nội dung của thẻ P");
 
 // const div = React.createElement("div", null, h1, p)
 // console.log(div)
-const container =  React.createElement(React.Fragment , null, h1, p)
-ReactDOM.render(container, document.querySelector("#root"))
+// const container =  React.createElement(React.Fragment , null, h1, p)
+// const ul = React.createElement(
+//         "ul",
+//         {
+//             className: "menu"
+//         },
+//         React.createElement("li", { className:"menu-item"}, "Bia"),
+//         React.createElement("li", { className:"menu-item"}, "Nước ngọt"),
+//         React.createElement("li", { className:"menu-item"}, "Nước ép"),
+//         )
+// ReactDOM.render(ul, document.querySelector("#root"))
 
 // Destructuring
 // let numbers = [10, 20, 30];
@@ -41,3 +50,103 @@ ReactDOM.render(container, document.querySelector("#root"))
 // let { hello, greeting } = khoa;
 // hello();
 // greeting();
+
+// JSX ~ JavaScript XML 
+// XML/ HTML
+
+// Component
+// Hook
+// 1. function component
+// 2. class component
+
+const drinks = [
+    "Bia",
+    "Nước ngọt",
+    "Nước ép",
+    "Nước có ga"
+]
+// let ulReact = (
+//     <ul className="menu">
+//         {
+//             drinks.map( (drink, index) => 
+//                 <li 
+//                     style={{
+//                         color:"white",
+//                         backgroundColor: "green"
+//                     }}
+//                     key={index} 
+//                     className="menu-item"
+//                     onClick = {() => alert(drink)}
+//                 >{drink}</li>
+//             )
+//         }
+//     </ul>
+// )
+// ReactDOM.render(ulReact, document.querySelector("#root"))
+
+// function List({data, color, children}){
+//     console.log(children)
+//     return (
+//         <ul>
+//             {
+//                 data.map((drink, index) => (
+//                     <li style ={
+//                         {
+//                             color: color
+//                         }
+//                     } key={index}>{drink}</li>
+//                 ))
+//             }
+//         </ul>
+//     )
+// }
+// ReactDOM.render(<List data={drinks} color="red">My List</List>, document.querySelector("#root"))
+
+// class List extends React.Component {
+//     render() {
+//         return (
+//             <ul>
+//                 {
+//                     this.props.data.map((drink, index) => (
+//                         <li style={
+//                             {
+//                                 color: this.props.color
+//                             }
+//                         } key={index}>{drink}</li>
+//                     ))
+//                 }
+//             </ul>
+//         )
+//     }
+// }
+
+// ReactDOM.render(<List data={drinks} color="red" />, document.querySelector("#root"))
+
+// One way binding
+// Two way binding
+
+function Input({ type, placeholder, onInput }){
+    return (
+        <input 
+            type = {type} 
+            placeholder={placeholder}
+            onInput = {onInput}
+         />
+    )
+}
+
+function handleInput(e){
+    console.log(e)
+}
+ReactDOM.render(<Input 
+                    type="text" 
+                    placeholder="Enter name..."
+                    onInput = {handleInput} />, 
+                document.querySelector("#root"))
+
+function f2(a){
+
+}
+function f1(a){
+    f2(a)
+}
